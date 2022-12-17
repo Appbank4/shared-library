5e2f345a0c83faa963e791bf22c916ab01a24bb1
Appbank4 <118092670+Appbank4@users.noreply.github.com>
2022-12-16 21:53:24 -0500

    Update newApp.groovy

--- vars/newApp.groovy
+++ vars/newApp.groovy
@@ -7,11 +7,25 @@ def call(String repoUrl){
                     sh 'free -m'
                 }
             }
-            stage('2-system-analysis'){
-                steps{
-                    sh 'lsblk'
-                }
-            }
+           stage('2-parallel jobs'){
+				parallel{
+					stage('1-analysis'){
+						steps{
+							sh 'lscpu'
+						}
+					}
+					stage('2-parallel2-job'){
+						when {
+							branch 'feature'
+						}
+						steps{
+							sh 'df -h'
+							sh 'free -g'
+						}
+					}
+
+				}
+			}
             stage("Checkout Code") {
                steps{
                    git branch: 'main',
@@ -20,10 +34,10 @@ def call(String repoUrl){
            }
            stage('closing'){
             steps{
-                echo "Build my app"
+                echo "AppBank"
             }
            }
-           stage('clone'){
+           stage('Analysis'){
             steps{
                 sh 'free -g'
                 sh 'pwd'
